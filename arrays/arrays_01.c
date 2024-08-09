aec146505c64443de26df5d43595f3ba19db73bb
Saad Ahmed Siddiqui <142170496+Saad1926Q@users.noreply.github.com>
2024-08-09 15:14:22 +0530

    Update arrays_01.c

--- arrays/arrays_01.c
+++ arrays/arrays_01.c
@@ -1,4 +1,4 @@
-#Given an array of size N. The task is to find the maximum and the minimum element of the array using the minimum number of comparisons.
+//Given an array of size N. The task is to find the maximum and the minimum element of the array using the minimum number of comparisons.
 
 #include <stdio.h>
 
